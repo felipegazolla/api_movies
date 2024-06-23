00f4f80acc0e4fb0e44bbacd6d615d5a51165ab2
felipe <enggazolla@gmail.com>
2024-06-23 20:00:59 -0300

    add get method

--- src/server.js
+++ src/server.js
@@ -2,6 +2,10 @@ const express = require('express')
 
 const app = express()
 
+app.get("/", (request, response) => {
+    
+})
+
 const PORT = 3333
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`))
