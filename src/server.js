b1e3ec04cc7bcdb48405c14856b9c0f05616dbe4
felipe <enggazolla@gmail.com>
2024-06-23 22:20:18 -0300

    userroutes

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 const express = require('express')
 
-const app = express()
+const routes = require("./routes")
 
-app.get("/", (request, response) => {
-    
-})
+const app = express()
+app.use(express.json())
+
+app.use(routes)
 
 const PORT = 3333
-
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
